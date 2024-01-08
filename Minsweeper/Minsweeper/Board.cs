af328185fa059c2d62a3df8e7d55249d2b502735
[samia <samiabari5.sb@gmail.com>
2024-01-09 04:09:27 +0800

    New solution Added

---
+++ Minsweeper/Minsweeper/Board.cs
@@ -0,0 +1,216 @@
+﻿namespace Minsweeper
+{
+    class Board
+    {
+        private int[,] grid;
+        private bool[,] revealedSquare;
+        private int gridSize;
+        private int numberOfMines;
+        private int mineLessSquare;
+        private int userInputCount = 0;
+        private int selectedRow;
+        private int selectedCol;
+        public Board(int size, int mines)
+        {
+            gridSize = size;
+            numberOfMines = mines;
+            grid = new int[size, size];
+            revealedSquare = new bool[size, size];
+            mineLessSquare = size * size - mines;
+
+            CreateGrid();
+            PlaceMines();
+        }
+
+        private void CreateGrid()
+        {
+            for (int i = 0; i < gridSize; i++)
+            {
+                for (int j = 0; j < gridSize; j++)
+                {
+                    grid[i, j] = 0;
+                    revealedSquare[i, j] = false;
+                }
+            }
+        }
+
+        private void PlaceMines()
+        {
+            Random random = new Random();
+
+            int minesPlaced = 0;
+            while (minesPlaced < numberOfMines)
+            {
+                int x = random.Next(gridSize);
+                int y = random.Next(gridSize);
+
+                if (grid[x, y] != -1)
+                {
+                    grid[x, y] = -1;
+                    IncrementAdjacentSquares(x, y);
+                    minesPlaced++;
+                }
+            }
+        }
+
+        private void IncrementAdjacentSquares(int x, int y)
+        {
+            int startX = Math.Max(0, x - 1);
+            int startY = Math.Max(0, y - 1);
+            int endX = Math.Min(gridSize - 1, x + 1);
+            int endY = Math.Min(gridSize - 1, y + 1);
+
+            for (int i = startX; i <= endX; i++)
+            {
+                for (int j = startY; j <= endY; j++)
+                {
+                    if (grid[i, j] != -1)
+                    {
+                        grid[i, j]++;
+                    }
+                }
+            }
+        }
+
+        public void Start()
+        {
+            string selectedSquare;
+            while (mineLessSquare > 0)
+            {
+
+                DisplayGrid();
+                userInputCount = 1;
+                
+                do {
+                    Console.Write("\nSelect a square to reveal (e.g. A1): ");
+                    selectedSquare = Console.ReadLine().ToUpper();
+                } while (!CheckValidSquareInput(selectedSquare));
+                
+                if (revealedSquare[selectedRow, selectedCol])
+                {
+                    Console.WriteLine("This square has already been uncovered.");
+                    continue;
+                }
+
+                if (grid[selectedRow, selectedCol] == -1)
+                {
+                    userInputCount = 0;
+                    Console.WriteLine("Oh no, you detonated a mine! Game over.");
+                    return;
+                }
+
+                RevealSquare(selectedRow, selectedCol);
+            }
+           
+            DisplayGrid();
+            userInputCount = 0;
+            Console.WriteLine("\nCongratulations, you have won the game!");
+        }
+
+        private bool CheckValidSquareInput(string input)
+        {
+            if (input.Length != 2 || !char.IsLetter(input[0]) || !char.IsDigit(input[1]))
+            {
+                Console.WriteLine("Incorrect input.");
+                return false;
+            }
+            else {
+                int x = input[0] - 'A';
+                int y = input[1] - '1';
+
+                if (x < 0 || x >= gridSize || y < 0 || y >= gridSize)
+                {
+                    Console.WriteLine("Incorrect input, Out of grid Size");
+                    return false;
+                }
+                else {
+                    selectedRow = x;
+                    selectedCol = y;
+                    return true;
+                }
+            }
+        }
+
+        
+
+        private void RevealSquare(int x, int y)
+        {
+            if (revealedSquare[selectedRow, selectedCol])
+            {
+                return;
+            }
+
+            revealedSquare[x, y] = true;
+            
+            if (grid[x, y] != -1)
+            {
+                Console.WriteLine("This square contains " + grid[x, y] + " adjacent mines.");
+            }
+            
+            mineLessSquare--;
+
+            if (grid[x, y] == 0)
+            {
+                int startX = Math.Max(0, x - 1);
+                int startY = Math.Max(0, y - 1);
+                int endX = Math.Min(gridSize - 1, x + 1);
+                int endY = Math.Min(gridSize - 1, y + 1);
+
+                for (int i = startX; i <= endX; i++)
+                {
+                    for (int j = startY; j <= endY; j++)
+                    {
+                        RevealSquare(i, j);
+                    }
+                }
+            }
+        }
+
+        private void DisplayGrid()
+        {
+
+            if (userInputCount == 0)
+            {
+                Console.WriteLine("\nHere is your minefield:");
+            }
+            else
+            {
+                Console.WriteLine("\nHere is your updated minefield:");
+            }
+            Console.Write("  ");
+
+            for (int i = 1; i <= gridSize; i++)
+            {
+                Console.Write(i + " ");
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < gridSize; i++)
+            {
+                Console.Write((char)('A' + i) + " ");
+
+                for (int j = 0; j < gridSize; j++)
+                {
+                    if (revealedSquare[i, j])
+                    {
+                        if (grid[i, j] == -1)
+                        {
+                            Console.Write("* ");
+                        }
+                        else
+                        {
+                            Console.Write(grid[i, j] + " ");
+                        }
+                    }
+                    else
+                    {
+                        Console.Write("_ ");
+                    }
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Minsweeper/Minsweeper/Minsweeper.cs
@@ -0,0 +1,150 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Minsweeper
+{
+    class Minsweeper
+    {
+        string pattern = @"^-?\d+$";
+        int gridSize;
+        int numberOfMines;
+        public void StartGame()
+        {
+            while (true)
+            {
+
+                InitialInput();
+                Board game = new Board(gridSize, numberOfMines);
+                game.Start();
+
+                Console.WriteLine("Press any key to play again...");
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+
+        public void InitialInput()
+        {
+            string input;
+            Console.WriteLine("Welcome to Minesweeper!");
+            do
+            {
+                Console.WriteLine("Enter the size of the grid (e.g. 4 for a 4x4 grid): ");
+                input = Console.ReadLine();
+            } while (!CheckValidIntNGridSize(input));
+            
+            do
+            {
+                Console.WriteLine("Enter the number of mines to place on the grid (maximum is 35% of the total squares): ");
+                input = Console.ReadLine();
+            } while (!CheckValidIntNMineNumber(input));
+
+        }
+
+        public bool CheckInputIsInt(string input)
+        {
+            bool result = true;
+            if (String.IsNullOrEmpty(input))
+            {
+                result = false;
+                Console.WriteLine("Incorrect input");
+
+            }
+            else if (!Regex.IsMatch(input, pattern))
+            {
+                result = false;
+                Console.WriteLine("Incorrect input");
+
+            }
+            return result;
+
+        }
+        private bool CheckValidIntNGridSize(string input)
+        {
+            int size;
+            if (CheckInputIsInt(input))
+            {
+                size = Convert.ToInt32(input);
+                if (CheckValidGridSize(size))
+                {
+                    gridSize = size;
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        private bool CheckValidIntNMineNumber(string input)
+        {
+            int number;
+            if (CheckInputIsInt(input))
+            {
+                number = Convert.ToInt32(input);
+                if (CheckValidMineNumber(number))
+                {
+                    numberOfMines = number;
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private bool CheckValidGridSize(int size)
+        {
+            if (size < 2)
+            {
+                Console.WriteLine("Minimum size of grid is 2.");
+                return false;
+            }
+            else if (size > 10)
+            {
+                Console.WriteLine("Maximum size of grid is 10.");
+                return false;
+            }
+            else {
+                return true;
+            }
+        }
+
+        private bool CheckValidMineNumber(int number)
+        {
+            int totalSquares = gridSize * gridSize;
+            double maxMineDouble= totalSquares * (35.0 / 100);
+            int maximumMineNumber = (int)Math.Round(maxMineDouble);
+            if (maximumMineNumber < 1)
+            {
+                maximumMineNumber= 1;
+            }
+            
+            if (number <= 0)
+            {
+                Console.WriteLine("There must be at least 1 mine.");
+                return false;
+            }
+            else if (number > maximumMineNumber)
+            {
+                Console.WriteLine("Maximum number is 35% of total sqaures.");
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+       
+    }
+}
