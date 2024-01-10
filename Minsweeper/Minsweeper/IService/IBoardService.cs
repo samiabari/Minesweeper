b171a4b79ee394cd0c4a00b30ae0e7f569c59185
[samia <32800475+samiabari@users.noreply.github.com>
2024-01-11 03:06:43 +0800

    comment added for IBoardService

--- Minsweeper/Minsweeper/IService/IBoardService.cs
+++ Minsweeper/Minsweeper/IService/IBoardService.cs
@@ -1,7 +1,18 @@
-﻿namespace Minsweeper.IService
+﻿/// <author>Samia Bari</author>
+/// <file>IBoardService.cs</file>
+/// <summary>
+/// Description: This interface provides method to start game play .
+/// </summary>
+namespace Minsweeper.IService
 {
+    /// <summary>
+    /// This interface provides method to start game play by letting users to take square input.
+    /// </summary>
     public interface IBoardService
     {
-        void Start();
+        /// <summary>
+        /// this method contains the functionality of letting users to take square input.
+        /// </summary>
+        void StartGame();
     }
 }
