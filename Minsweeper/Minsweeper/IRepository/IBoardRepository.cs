d9b776e80cc45e304a06e8bc8a2cc646b825509c
[samia <32800475+samiabari@users.noreply.github.com>
2024-01-11 02:51:36 +0800

    Comments added for IBoardRepository.cs

--- Minsweeper/Minsweeper/IRepository/IBoardRepository.cs
+++ Minsweeper/Minsweeper/IRepository/IBoardRepository.cs
@@ -1,8 +1,29 @@
-﻿namespace Minsweeper.IRepository
+﻿/// <author>Samia Bari</author>
+/// <file>IBoardRepository.cs</file>
+/// <summary>
+/// Description: This interface provides methods to create the game grid and place mines within it.
+/// </summary>
+namespace Minsweeper.IRepository
 {
+    /// <summary>
+    /// Interface to create the board/grid by adding mines and adjacent square values for the Minesweeper game.
+    /// </summary>
     public interface IBoardRepository
     {
+        /// <summary>
+        /// Creates the grid as per user's given grid size. and initialize it by 0 and setting unrevealed.
+        /// </summary>
+        /// <param name="size"> size of the grid. </param>
+        /// <param name="grid"> two-dimensional array to represent the game board.</param>
+        /// <param name="revealedSquare"> two-dimensional array to represent the revealed square, same size as the grid size. </param>
         void CreateGrid(int size, int[,] grid, bool[,] revealedSquare);
+
+        /// <summary>
+        /// initially places the mines randomely and increase the values of adjacent squares.
+        /// </summary>
+        /// <param name="grid"> two-dimensional array to represent the game board. </param>
+        /// <param name="revealedSquare"> two-dimensional array to represent the revealed square, same size as the grid size. </param>
+        /// <param name="numberOfMines"> Number of mines given by the user. </param>
         void PlaceMines(int[,] grid, bool[,] revealedSquare, int numberOfMines);
     }
 }
